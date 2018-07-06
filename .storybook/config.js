680329dbe524328b44a4199ad4cd297d61e02916
xinghunm <xinghunm@curacloudcorp.com>
2018-07-06 18:27:04 +0800

    import all stories files  without explicitly requiring one file

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,11 +3,13 @@ import chaptersAddon, { setDefaults } from 'react-storybook-addon-chapters';
 import { withKnobs } from '@storybook/addon-knobs/react';
 
 setAddon(chaptersAddon);
-setDefaults({sectionOptions: {useTheme: false}});
-function loadStories() {
-  require('../src/stories');
-}
-
 addDecorator(withKnobs);
+setDefaults({sectionOptions: {useTheme: false}});
+
+const req = require.context('../src', true, /_story\.(js|jsx)$/)
+
+function loadStories() {
+  req.keys().forEach(req)
+}
 
 configure(loadStories, module);
