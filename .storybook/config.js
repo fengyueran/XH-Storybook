6b020c57ead6b734a095f927ae179cf000227c10
xinghunm <xinghunm@curacloudcorp.com>
2018-07-16 15:56:15 +0800

    update story regex

--- .storybook/config.js
+++ .storybook/config.js
@@ -6,7 +6,7 @@ setAddon(chaptersAddon);
 addDecorator(withKnobs);
 setDefaults({sectionOptions: {useTheme: false}});
 
-const req = require.context('../src', true, /_story\.(js|jsx)$/)
+const req = require.context('../src', true, /__story__\.(js|jsx)$/)
 
 function loadStories() {
   req.keys().forEach(req)
